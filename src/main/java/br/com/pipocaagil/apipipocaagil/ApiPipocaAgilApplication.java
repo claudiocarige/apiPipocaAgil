2049bec8d51df36a8a6cb2887be54083b8bbf946
Claudio Carige <ccarige@gmail.com>
2023-09-28 20:53:53 -0300

    First commit

---
+++ src/main/java/br/com/pipocaagil/apipipocaagil/ApiPipocaAgilApplication.java
@@ -0,0 +1,13 @@
+package br.com.pipocaagil.apipipocaagil;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiPipocaAgilApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiPipocaAgilApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/com/pipocaagil/apipipocaagil/ApiPipocaAgilApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.pipocaagil.apipipocaagil;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiPipocaAgilApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
