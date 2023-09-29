c0507677bb3490b25b9b76eb3959806668a46518
Claudio Carige <ccarige@gmail.com>
2023-09-28 22:28:19 -0300

    Created UsersServiceImpl class

---
+++ src/main/java/br/com/pipocaagil/apipipocaagil/services/impl/UsersServiceImpl.java
@@ -0,0 +1,35 @@
+package br.com.pipocaagil.apipipocaagil.services.impl;
+
+import br.com.pipocaagil.apipipocaagil.domain.Users;
+import br.com.pipocaagil.apipipocaagil.services.UsersService;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UsersServiceImpl implements UsersService {
+    @Override
+    public Users findById(Long id) {
+        return null;
+    }
+
+    @Override
+    public List<Users> findAll() {
+        return null;
+    }
+
+    @Override
+    public Users insert(Users user) {
+        return null;
+    }
+
+    @Override
+    public Users update(Long id, Users user) {
+        return null;
+    }
+
+    @Override
+    public void delete(Long id) {
+
+    }
+}
