fee40fc68c94ca1eb8019128b87a82742fa22ff2
Claudio Carige <ccarige@gmail.com>
2023-09-28 23:41:51 -0300

    Implemented the findById and findAll methods

--- src/main/java/br/com/pipocaagil/apipipocaagil/controllers/UsersController.java
+++ src/main/java/br/com/pipocaagil/apipipocaagil/controllers/UsersController.java
@@ -1,17 +1,30 @@
 package br.com.pipocaagil.apipipocaagil.controllers;
 
+import br.com.pipocaagil.apipipocaagil.domain.Users;
+import br.com.pipocaagil.apipipocaagil.services.UsersService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 @RequestMapping(value = "/users")
+@RequiredArgsConstructor
 public class UsersController {
 
-    @GetMapping
-    public ResponseEntity<String> getUser(){
-        String str = new String("{Pipoca Àgil}");
-        return ResponseEntity.ok().body(str);
+    private final UsersService userService;
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<Users> findById(@PathVariable Long id){
+        return ResponseEntity.ok().body(userService.findById(id));
+    }
+
+    @GetMapping()
+    public ResponseEntity<List<Users>> findAll(){
+        return ResponseEntity.ok().body(userService.findAll());
     }
 }
--- src/main/java/br/com/pipocaagil/apipipocaagil/services/impl/UsersServiceImpl.java
+++ src/main/java/br/com/pipocaagil/apipipocaagil/services/impl/UsersServiceImpl.java
@@ -1,21 +1,29 @@
 package br.com.pipocaagil.apipipocaagil.services.impl;
 
 import br.com.pipocaagil.apipipocaagil.domain.Users;
+import br.com.pipocaagil.apipipocaagil.repositories.UsersRepository;
 import br.com.pipocaagil.apipipocaagil.services.UsersService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
+@RequiredArgsConstructor
 public class UsersServiceImpl implements UsersService {
+
+    private final UsersRepository userRepository;
+
     @Override
     public Users findById(Long id) {
-        return null;
+        Optional<Users> user = userRepository.findById(id);
+        return user.orElseThrow();
     }
 
     @Override
     public List<Users> findAll() {
-        return null;
+        return userRepository.findAll();
     }
 
     @Override
