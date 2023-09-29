6e88559849a66070b77e22bfc93749a71b9e69f5
Claudio Carige <ccarige@gmail.com>
2023-09-28 23:03:58 -0300

    TestConfig implementation

--- src/main/java/br/com/pipocaagil/apipipocaagil/config/TestConfig.java
+++ src/main/java/br/com/pipocaagil/apipipocaagil/config/TestConfig.java
@@ -1,14 +1,20 @@
 package br.com.pipocaagil.apipipocaagil.config;
 
+import br.com.pipocaagil.apipipocaagil.services.DBService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
 @Configuration
 @Profile("test")
+@RequiredArgsConstructor
 public class TestConfig {
+
+    private final DBService dbService;
+
     @Bean
-    public void startDB(){
-        // TODO document why this method is empty
+    public void startDB() {
+        this.dbService.startDB();
     }
 }
