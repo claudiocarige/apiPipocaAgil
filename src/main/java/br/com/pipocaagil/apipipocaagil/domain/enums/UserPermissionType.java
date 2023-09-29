cd834e8a56415590077ecdefeca49d5c8318e8e5
Claudio Carige <ccarige@gmail.com>
2023-09-29 13:48:47 -0300

    Customize enums fields

--- src/main/java/br/com/pipocaagil/apipipocaagil/domain/enums/UserPermissionType.java
+++ src/main/java/br/com/pipocaagil/apipipocaagil/domain/enums/UserPermissionType.java
@@ -1,5 +1,32 @@
 package br.com.pipocaagil.apipipocaagil.domain.enums;
 
 public enum UserPermissionType {
-    ADMIN, USER;
+    ADMIN(0, "ROLE_ADMIN"), USER(1, "ROLE_USER");
+
+    private Integer codigo;
+    private String descricao;
+
+    UserPermissionType(Integer codigo, String descricao) {
+        this.codigo = codigo;
+        this.descricao = descricao;
+    }
+
+    public Integer getCodigo() {
+        return codigo;
+    }
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public static UserPermissionType toEnum(Integer codigo) {
+        if(codigo == null) {
+            return null;
+        }
+        for(UserPermissionType x: UserPermissionType.values()) {
+            if(codigo.equals(x.getCodigo())) {
+                return x;
+            }
+        }
+        throw new IllegalArgumentException("Perfil inválido");
+    }
 }
