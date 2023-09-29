bf27d1694dd9cb29beffab3efcfcece27f2ac5df
Claudio Carige <ccarige@gmail.com>
2023-09-28 23:58:25 -0300

    Validation dependency inserted

---
+++ src/main/java/br/com/pipocaagil/apipipocaagil/domain/representations/UsersRepresentation.java
@@ -0,0 +1,34 @@
+package br.com.pipocaagil.apipipocaagil.domain.representations;
+
+import br.com.pipocaagil.apipipocaagil.domain.enums.UserPermissionType;
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.validation.constraints.NotNull;
+import java.time.LocalDate;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class UsersRepresentation {
+
+
+    private Long id;
+    @NotNull(message = "O campo NOME é requerido!")
+    private String name;
+    @NotNull(message = "O campo EMAIL é requerido!")
+    private String email;
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
+    @NotNull(message = "O campo SENHA é requerido!")
+    private String password;
+    @JsonFormat(pattern = "dd/MM/yyyy")
+    private LocalDate birthday;
+    @JsonFormat(pattern = "dd/MM/yyyy")
+    private LocalDate createDate = LocalDate.now();
+    private UserPermissionType role;
+}
