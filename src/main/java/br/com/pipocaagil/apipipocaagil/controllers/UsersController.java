1573aa00cf1776bd61a58c3aef60885660bb5f0c
Claudio Carige <ccarige@gmail.com>
2023-10-17 16:21:08 -0300

    Refactor UsersController class

--- src/main/java/br/com/pipocaagil/apipipocaagil/controllers/UsersController.java
+++ src/main/java/br/com/pipocaagil/apipipocaagil/controllers/UsersController.java
@@ -1,5 +1,6 @@
 package br.com.pipocaagil.apipipocaagil.controllers;
 
+import br.com.pipocaagil.apipipocaagil.domain.Users;
 import br.com.pipocaagil.apipipocaagil.domain.representations.UsersRepresentation;
 import br.com.pipocaagil.apipipocaagil.services.UsersService;
 import lombok.RequiredArgsConstructor;
@@ -14,7 +15,7 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 @RestController
-@RequestMapping(value = "/users")
+@RequestMapping(value = "api/v1/users")
 @RequiredArgsConstructor
 public class UsersController {
 
@@ -26,36 +27,29 @@ public class UsersController {
         return ResponseEntity.ok().body(mapper.map(userService.findById(id), UsersRepresentation.class));
     }
 
-    @GetMapping(value = "/email")
-    public ResponseEntity<UsersRepresentation> findByEmail(@RequestParam("email") String email) {
-        return ResponseEntity.ok().body(mapper.map(userService.findByEmail(email), UsersRepresentation.class));
+    @GetMapping(value = "/username")
+    public ResponseEntity<UsersRepresentation> findByUsername(@RequestParam("username") String username) {
+        return ResponseEntity.ok().body(mapper.map(userService.findByUsername(username), UsersRepresentation.class));
     }
 
     @GetMapping()
     public ResponseEntity<List<UsersRepresentation>> findAll() {
-        return ResponseEntity.ok().body(userService.findAll()
-                .stream()
-                .map(x -> mapper.map(x, UsersRepresentation.class))
-                .collect(Collectors.toList()));
+        return ResponseEntity.ok().body(toRepresentation(userService.findAll()));
     }
 
     @GetMapping(value = "/name/firstname")
     public ResponseEntity<List<UsersRepresentation>> findByNameIgnoreCase(@RequestParam("firstName") String firstName) {
-        return ResponseEntity.ok().body(userService.findByNameIgnoreCase(firstName)
-                .stream()
-                .map(x -> mapper.map(x, UsersRepresentation.class))
-                .collect(Collectors.toList()));
+        return ResponseEntity.ok().body(toRepresentation(userService.findByNameIgnoreCase(firstName)));
     }
-    @GetMapping(value = "/firstname-or-lastname")
+
+    @GetMapping(value = "/firtname-lastname")
     public ResponseEntity<List<UsersRepresentation>> findByFirstNameContainingIgnoreCaseOrLastNameContainingIgnoreCase(@RequestParam("firstNamePart") String firstNamePart,
                                                                                                                        @RequestParam("lastNamePart") String lastNamePart) {
-        return ResponseEntity.ok().body(userService.findByFirstNameContainingIgnoreCaseOrLastNameContainingIgnoreCase(firstNamePart, lastNamePart)
-                .stream()
-                .map(x -> mapper.map(x, UsersRepresentation.class))
-                .collect(Collectors.toList()));
+        return ResponseEntity.ok().body(toRepresentation(userService.findByFirstNameContainingIgnoreCaseOrLastNameContainingIgnoreCase(firstNamePart, lastNamePart)));
     }
+
     @PostMapping()
-    public ResponseEntity<UsersRepresentation> insert(@Valid @RequestBody UsersRepresentation usersRepresentation){
+    public ResponseEntity<UsersRepresentation> insert(@Valid @RequestBody UsersRepresentation usersRepresentation) {
         URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("{id}")
                 .buildAndExpand(userService.insert(usersRepresentation).getId()).toUri();
         return ResponseEntity.created(uri).build();
@@ -63,14 +57,21 @@ public class UsersController {
 
     @PutMapping(value = "/{id}")
     public ResponseEntity<UsersRepresentation> update(@PathVariable Long id,
-                                                      @Valid @RequestBody UsersRepresentation usersRepresentation){
+                                                      @Valid @RequestBody UsersRepresentation usersRepresentation) {
         return ResponseEntity.ok().body(mapper.map(userService.update(id, usersRepresentation),
-                                                                                        UsersRepresentation.class));
+                UsersRepresentation.class));
     }
 
     @DeleteMapping(value = "/{id}")
-    public ResponseEntity<UsersRepresentation> delete(@PathVariable Long id){
+    public ResponseEntity<UsersRepresentation> delete(@PathVariable Long id) {
         userService.delete(id);
         return ResponseEntity.noContent().build();
+    }
+
+    private List<UsersRepresentation> toRepresentation(List<Users> users) {
+        return users
+                .stream()
+                .map(x -> mapper.map(x, UsersRepresentation.class))
+                .collect(Collectors.toList());
     }
 }
