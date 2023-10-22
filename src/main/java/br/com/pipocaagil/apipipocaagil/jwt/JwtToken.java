141c8afa405eac8df2bf1a8b0f8d18db092125d8
Claudio Carige <ccarige@gmail.com>
2023-10-22 00:01:12 -0300

    Created JwtToken and JwtUserDetails class

---
+++ src/main/java/br/com/pipocaagil/apipipocaagil/jwt/JwtToken.java
@@ -0,0 +1,15 @@
+package br.com.pipocaagil.apipipocaagil.jwt;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class JwtToken {
+
+    private String token;
+}
