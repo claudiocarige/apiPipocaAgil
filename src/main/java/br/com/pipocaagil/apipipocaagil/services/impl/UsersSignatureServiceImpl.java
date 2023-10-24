400a53a8271b0210aff379bd78b98b2afece392f
Claudio Carige <ccarige@gmail.com>
2023-10-23 22:44:25 -0300

    Created UsersSignatureServiceImpl class

---
+++ src/main/java/br/com/pipocaagil/apipipocaagil/services/impl/UsersSignatureServiceImpl.java
@@ -0,0 +1,28 @@
+package br.com.pipocaagil.apipipocaagil.services.impl;
+
+import br.com.pipocaagil.apipipocaagil.domain.enums.UserPermissionType;
+import br.com.pipocaagil.apipipocaagil.domain.representations.UserLoginRepresentation;
+import br.com.pipocaagil.apipipocaagil.services.UsersService;
+import br.com.pipocaagil.apipipocaagil.services.UsersSignatureService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+@Slf4j
+@RequiredArgsConstructor
+@Service
+public class UsersSignatureServiceImpl implements UsersSignatureService {
+
+    private final UsersService userService;
+
+    public void signatureToUser(UserLoginRepresentation userLoginRepresentation, UserPermissionType role){
+        userService.updateRoleToSigned(userLoginRepresentation, UserPermissionType.ROLE_SIGNED);
+        log.info("Signature to user");
+    }
+
+    public void toPayment(){
+        log.info("Payment");
+
+    }
+
+}
