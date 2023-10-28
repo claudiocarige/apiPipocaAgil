510498b1b4d8ad4f7856372f4bdaafcfc10c423a
Claudio Carige <ccarige@gmail.com>
2023-10-28 13:32:16 -0300

    Created PasswordUserService interface

---
+++ src/main/java/br/com/pipocaagil/apipipocaagil/services/interfaces/PasswordUserService.java
@@ -0,0 +1,10 @@
+package br.com.pipocaagil.apipipocaagil.services.interfaces;
+
+import br.com.pipocaagil.apipipocaagil.domain.representations.UserPasswordRepresentation;
+
+public interface PasswordUserService {
+
+    void updatePassword(Long id, UserPasswordRepresentation pass);
+    String resetPassword(String email);
+
+}
