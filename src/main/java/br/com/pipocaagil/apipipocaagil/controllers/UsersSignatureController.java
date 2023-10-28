e044e65768810ea5b83505809637db3bb922948f
Claudio Carige <ccarige@gmail.com>
2023-10-28 13:36:44 -0300

    Created OpanApi description and removed the checkCard method

--- src/main/java/br/com/pipocaagil/apipipocaagil/controllers/UsersSignatureController.java
+++ src/main/java/br/com/pipocaagil/apipipocaagil/controllers/UsersSignatureController.java
@@ -4,18 +4,21 @@ import br.com.pipocaagil.apipipocaagil.controllers.exceptions.PaymentAuthorizati
 import br.com.pipocaagil.apipipocaagil.domain.enums.UserPermissionType;
 import br.com.pipocaagil.apipipocaagil.domain.representations.CardDetailsRepresentation;
 import br.com.pipocaagil.apipipocaagil.domain.representations.UserLoginRepresentation;
+import br.com.pipocaagil.apipipocaagil.domain.representations.UserPasswordRepresentation;
 import br.com.pipocaagil.apipipocaagil.jwt.JwtToken;
 import br.com.pipocaagil.apipipocaagil.jwt.JwtUserDetailsService;
-import br.com.pipocaagil.apipipocaagil.services.interfaces.UsersService;
+import br.com.pipocaagil.apipipocaagil.services.impl.ContextCheckImpl;
 import br.com.pipocaagil.apipipocaagil.services.interfaces.UsersSignatureService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.access.AccessDeniedException;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -25,17 +28,31 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 @RestController
 @RequestMapping("/api/v1/users/signature")
+@Tag(name = "Signing users", description = "Contains all operations related to the resources Signing users.")
 public class UsersSignatureController {
 
     private final JwtUserDetailsService userDetailsService;
     private final UsersSignatureService signatureService;
-    private final UsersService userService;
+    private final ContextCheckImpl contextCheck;
 
-    @PostMapping
+    @PostMapping(value = "/payment")
+    @Operation(summary = "Signing users ",
+            description = "Updates the user's password by passing a JSON representation of the user's old password and a new password and its confirmation.",
+            tags = {"Users"},
+            responses = {
+                    @ApiResponse(description = "Success", responseCode = "204",
+                            content = @Content(mediaType = "application/json", schema = @Schema(implementation = UserPasswordRepresentation.class))
+                    ),
+                    @ApiResponse(responseCode = "204", description = "No Content", content = @Content),
+                    @ApiResponse(responseCode = "400", description = "Bad Request", content = @Content),
+                    @ApiResponse(responseCode = "401", description = "Unauthorized", content = @Content),
+                    @ApiResponse(responseCode = "404", description = "Users not found", content = @Content),
+                    @ApiResponse(responseCode = "500", description = "Internal Error", content = @Content)
+            })
     public ResponseEntity<?> signatureToUser(
                  @RequestBody @Valid CardDetailsRepresentation cardDetailsRepresentation, HttpServletRequest request) {
 
-        UserLoginRepresentation userLoginRepresentation = checkUser();
+        UserLoginRepresentation userLoginRepresentation = contextCheck.checkUser();
         if (!checkCard(cardDetailsRepresentation)){
             log.info("Unauthorized Card!");
             throw new PaymentAuthorizationException("Unauthorized Card!");
@@ -46,15 +63,7 @@ public class UsersSignatureController {
         return ResponseEntity.ok(token);
     }
 
-    private UserLoginRepresentation checkUser(){
-        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-        var username = authentication.getName();
-        var user = userService.findByUsername(username);
-        if (username == null){
-            throw new AccessDeniedException("Access Denied. You must be logged in for this operation!");
-        }
-        return new UserLoginRepresentation(user.getUsername(), user.getPassword());
-    }
+
     private static boolean checkCard(CardDetailsRepresentation cardDetailsRepresentation){
         var cardNumber = "4929857104078491";
         var cardHolderName = "Pipoca Agil";
