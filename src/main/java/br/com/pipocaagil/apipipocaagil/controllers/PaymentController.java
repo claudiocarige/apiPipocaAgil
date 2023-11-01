502272539be528ecd8cd45c9080e485e6ee38fdf
Claudio Carige <ccarige@gmail.com>
2023-11-01 16:41:25 -0300

    Created PaymentController Implementation

---
+++ src/main/java/br/com/pipocaagil/apipipocaagil/controllers/PaymentController.java
@@ -0,0 +1,28 @@
+package br.com.pipocaagil.apipipocaagil.controllers;
+
+import br.com.pipocaagil.apipipocaagil.payments.interfaces.PaymentService;
+import br.com.pipocaagil.apipipocaagil.payments.representations.OrderRepresentation;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@Slf4j
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/api/v1/payment")
+public class PaymentController {
+
+    private final PaymentService paymentService;
+
+    @PostMapping("/card")
+    public ResponseEntity<Object>createPayment(@RequestBody OrderRepresentation order){
+        var payment = paymentService.createPayment(order);
+        log.info("Payment created: {}", payment);
+        return ResponseEntity.ok(payment);
+    }
+}
