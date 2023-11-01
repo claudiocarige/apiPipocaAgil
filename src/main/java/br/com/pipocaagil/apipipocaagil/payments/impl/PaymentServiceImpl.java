3f7f102b43cf9a4975317a690cc4cf38a92286a2
Claudio Carige <ccarige@gmail.com>
2023-11-01 17:19:53 -0300

    Refactor PaymentServiceImpl Implementation

--- src/main/java/br/com/pipocaagil/apipipocaagil/payments/impl/PaymentServiceImpl.java
+++ src/main/java/br/com/pipocaagil/apipipocaagil/payments/impl/PaymentServiceImpl.java
@@ -3,17 +3,19 @@ package br.com.pipocaagil.apipipocaagil.payments.impl;
 import br.com.pipocaagil.apipipocaagil.payments.interfaces.PaymentService;
 import br.com.pipocaagil.apipipocaagil.payments.representations.OrderRepresentation;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.*;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
+@Slf4j
 @RequiredArgsConstructor
 @Service
 public class PaymentServiceImpl implements PaymentService {
 
     @Value("${pagseguro.token}")
-    private String TOKEN;
+    private String TOKEN_KEY;
 
     private final RestTemplate restTemplate;
 
@@ -22,13 +24,13 @@ public class PaymentServiceImpl implements PaymentService {
         var url = "https://sandbox.api.pagseguro.com/orders";
         var headers = new HttpHeaders();
         headers.setContentType(MediaType.APPLICATION_JSON);
-        headers.set("Authorization", "Bearer " + TOKEN);
+        headers.set("Authorization", "Bearer " + TOKEN_KEY);
         headers.set("accept", "application/json");
 
         HttpEntity<Object> entity = new HttpEntity<>(order, headers);
 
-        ResponseEntity<Object> response = restTemplate.exchange(url, HttpMethod.POST, entity, Object.class);
-
+        var response = restTemplate.exchange(url, HttpMethod.POST, entity, Object.class);
+        log.info("Log info Response:  %s" +  response.getBody().toString());
         return response.getBody();
     }
 }
