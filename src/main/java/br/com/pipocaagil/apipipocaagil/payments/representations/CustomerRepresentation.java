ee6d4fdab90dd64018a45f2010a55c89af9fb544
Claudio Carige <ccarige@gmail.com>
2023-11-02 20:30:24 -0300

    Created getEmail method

--- src/main/java/br/com/pipocaagil/apipipocaagil/payments/representations/CustomerRepresentation.java
+++ src/main/java/br/com/pipocaagil/apipipocaagil/payments/representations/CustomerRepresentation.java
@@ -2,4 +2,8 @@ package br.com.pipocaagil.apipipocaagil.payments.representations;
 
 import java.util.List;
 
-public record CustomerRepresentation(String name, String email, String tax_id, List<PhonesRepresentation> phones) {}
+public record CustomerRepresentation(String name, String email, String tax_id, List<PhonesRepresentation> phones) {
+    public String getEmail() {
+        return email;
+    }
+}
