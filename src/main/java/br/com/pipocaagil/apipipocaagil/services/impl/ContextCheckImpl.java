f5f6a7a4f9366ff2c6cfc0484d43105761055fb3
Claudio Carige <ccarige@gmail.com>
2023-12-12 20:24:43 -0300

    Refactored ContextCheckImpl class

--- src/main/java/br/com/pipocaagil/apipipocaagil/services/impl/ContextCheckImpl.java
+++ src/main/java/br/com/pipocaagil/apipipocaagil/services/impl/ContextCheckImpl.java
@@ -1,18 +1,17 @@
 package br.com.pipocaagil.apipipocaagil.services.impl;
 
-import br.com.pipocaagil.apipipocaagil.domain.representations.UserLoginRepresentation;
 import br.com.pipocaagil.apipipocaagil.services.interfaces.UsersService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.access.AccessDeniedException;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
 
 @Slf4j
 @RequiredArgsConstructor
-@Component
-public class ContextCheckImpl{
+@Service
+public final class ContextCheckImpl{
 
     private final UsersService userService;
     public void checkUser(Long id){
@@ -25,15 +24,5 @@ public class ContextCheckImpl{
                 throw new AccessDeniedException("Access denied, You're not authorized to modify this User");
             }
         }
-    }
-    public UserLoginRepresentation checkUser(){
-        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-        var username = authentication.getName();
-        log.info("Checking user {} ", username);
-        var user = userService.findByUsername(username);
-        if (username == null){
-            throw new AccessDeniedException("Access Denied. You must be logged in for this operation!");
-        }
-        return new UserLoginRepresentation(user.getUsername(), user.getPassword());
     }
 }
